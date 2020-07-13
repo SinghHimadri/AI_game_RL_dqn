d5c655f78b81573781ab1f40ea67fd0b9b0df484
SinghHimadri <himsingh1612@gmail.com>
2020-07-13 23:51:05 +0530

    train model for mountain car

--- mountain_car.py
+++ mountain_car.py
@@ -86,3 +86,63 @@ class DQNSolver:
             self.model.fit(state, q_values, verbose=0)
         self.exploration_rate *= EXPLORATION_DECAY
         self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
+
+
+# determine reward of action
+# note: i think this get_reward method could use some improvement since many of the solutions
+# i've seen online work a lot faster than mine, so this is definitely not optimal
+def get_reward(state):
+    if state[0] >= 0.5:
+        print("Car has reached checkpoint!")
+        return 10 # reward success
+    if state[0] > -0.4:
+        # punish falling off the other edge
+        return (1+state[0])**2
+    return 0
+
+def mountaincar():
+    # initialize game and score logger
+    env = gym.make(ENV_NAME)
+    # tool created to display 'score'
+    score_logger = ScoreLogger(ENV_NAME)
+    observation_space = env.observation_space.shape[0]
+    action_space = env.action_space.n
+    dqn_solver = DQNSolver(observation_space, action_space)
+    run = 0
+    score = 0
+    numTries = 200
+
+    # continue indefinitely to train and perfect the mdoel
+    while run < numTries:
+        run += 1
+        # a new frame
+        state = env.reset()
+        state = np.reshape(state, [1, observation_space])
+        step = 0
+
+        # keep stepping til you fall out of frame
+        while True:
+            # more steps = more successful (even if it is moving)
+            env.render()
+
+            # choose what to do using DQN
+            action = dqn_solver.act(state)
+            # analyze what happened
+            state_next, reward, dead, info = env.step(action)
+
+            # reinforce positive outcome, penalize bad outcome
+            reward = get_reward(state_next)
+            step += reward
+
+            state_next = np.reshape(state_next, [1, observation_space])
+            # memorize this iteration to shape the following
+            dqn_solver.memorize(state, action, reward, state_next, dead)
+            state = state_next
+            if dead:
+                # score = # of steps taken in a particular run (too many steps is bad)
+                print ("Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step))
+                score_logger.add_score(step, run)
+                break
+            dqn_solver.replay()
+
+mountaincar()
