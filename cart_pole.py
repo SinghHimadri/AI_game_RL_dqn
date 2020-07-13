2514f938202e717e8c44eec1d1dc5d5ab5bdd8ef
SinghHimadri <himsingh1612@gmail.com>
2020-07-13 21:17:04 +0530

    DQN final added for mountain car game

--- cart_pole.py
+++ cart_pole.py
@@ -6,6 +6,7 @@ from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+# from scores.score_logger import ScoreLogger
 # region END
 
 
