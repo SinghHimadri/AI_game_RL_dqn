de7a9201dfe2001c5d916ff0815944d9863b2645
SinghHimadri <himsingh1612@gmail.com>
2020-07-14 23:35:33 +0530

    main started

--- pong_V0.py
+++ pong_V0.py
@@ -80,3 +80,63 @@ def apply_neural_nets(observation_matrix, weights):
     output_layer_values = np.dot(hidden_layer_values, weights['2'])
     output_layer_values = sigmoid(output_layer_values)
     return hidden_layer_values, output_layer_values
+
+
+def main():
+    # initialize ping pong game (atari style)
+    env = gym.make("Pong-v0")
+
+    # get the initial game image
+    observation = env.reset()
+
+    # hyperparameters
+    # some global variables to fine-tune the learning metrics
+    episode_number = 0
+    batch_size = 10
+    gamma = 0.99 # discount factor for reward
+    decay_rate = 0.99
+    num_hidden_layer_neurons = 200
+    input_dimensions = 80 * 80
+    learning_rate = 1e-4
+
+    episode_number = 0
+    reward_sum = 0
+    running_reward = None
+    prev_processed_observations = None
+
+    # weights to determine number of neurons and their relative importance
+    weights = {
+        '1': np.random.randn(num_hidden_layer_neurons, input_dimensions) / np.sqrt(input_dimensions),
+        '2': np.random.randn(num_hidden_layer_neurons) / np.sqrt(num_hidden_layer_neurons)
+    }
+
+    # To be used with rmsprop algorithm (http://sebastianruder.com/optimizing-gradient-descent/index.html#rmsprop)
+    expectation_g_squared = {}
+    g_dict = {}
+    for layer_name in weights.keys():
+        expectation_g_squared[layer_name] = np.zeros_like(weights[layer_name])
+        g_dict[layer_name] = np.zeros_like(weights[layer_name])
+
+    episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], []
+
+    # infinite game loop (need to change) to run neural network and choose an action to take
+    while True:
+        env.render()
+
+        # generate observations for this and previous frame (trim down data and only give difference between frames)
+        processed_observations, prev_processed_observations = preprocess_observations(observation, prev_processed_observations, input_dimensions)
+        # compute hidden layer and probability of going up by running neural net
+        hidden_layer_values, up_probability = apply_neural_nets(processed_observations, weights)
+
+        # collect observations across the 'episode'
+        episode_observations.append(processed_observations)
+        episode_hidden_layer_values.append(hidden_layer_values)
+
+        # choose whether to move up or down by 'flipping a coin' given the determined probability
+        action = choose_action(up_probability)
+
+        # carry out the chosen action
+        observation, reward, done, info = env.step(action)
+
+        reward_sum += reward
+        episode_rewards.append(reward)
